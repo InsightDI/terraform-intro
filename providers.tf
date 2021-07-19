954a42381d549c819c35322a4b3362d027f4f6bf
Dave Benedic <dave.benedic@insight.com>
2021-07-19 19:46:37 +0000

    format

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     azurerm = {
-      source = "registry.terraform.io/hashicorp/azurerm"
+      source  = "registry.terraform.io/hashicorp/azurerm"
       version = "2.68.0"
     }
   }
